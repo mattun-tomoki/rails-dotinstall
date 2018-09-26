8a4859068d26fed90be4a6fde36350886d16b5dd
mattun <aosumi39@outlook.jp>
2018-09-26 03:23:11 +0000

    create comments routes and add comment function

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,3 @@
 class Post < ApplicationRecord
-  validates :title, presence: true , length: {minimum: 3,message: 'Too short to Post!' }
-  validates :body, presence: true
+  has_many :comments
 end
